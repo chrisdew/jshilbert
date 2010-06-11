f9f1296bd335a159d9b67fb887c2c9445df440d8
Chris Dew <cmsdew+github@gmail.com>
2010-06-11 12:35:05 +0100

    1d_to_2d now working

--- hilbert.js
+++ hilbert.js
@@ -32,6 +32,13 @@ Battenburg.prototype.rotate_c = function(){
 Battenburg.prototype.rotate_cc = function(){
 	return new Battenburg(this.b, this.d, this.a, this.c);
 }
+Battenburg.prototype.divide_by_scalar = function(div) {
+	return new Battenburg(this.a / div, this.b / div, this.c / div, this.d /div)
+}
+Battenburg.prototype.add = function(bb) {
+	return new Battenburg(this.a + bb.a, this.b + bb.b, this.c + bb.c, this.d + bb.d)
+}
+
 
 /**
  * This is recursive function for calculating the  
@@ -56,11 +63,9 @@ function hilbert_2d_to_1d(x, y, recursion, curve) {
 	if (curve === undefined) {
 		curve = new Battenburg(0.0, 0.25, 0.75, 0.5);
 	}
-	
 	if (recursion === undefined) {
 		recursion = 28;	// a good number for doubles
 	}
-	
 	if (recursion <= 0) {
 		return 0.0;
 	}
@@ -96,5 +101,44 @@ function hilbert_2d_to_1d(x, y, recursion, curve) {
 	}
 	//console.log(x, y, recursion, curve);	
 	//console.log("retv: " + retv);
+	return retv;
+}
+
+function hilbert_1d_to_2d(d, recursion, curve) {
+	console.log(d, recursion, curve);	
+	if (curve === undefined) {
+		curve = new Battenburg(0.0, 0.25, 0.75, 0.5);
+	}
+	if (recursion === undefined) {
+		recursion = 28;	// a good number for doubles
+	}
+	if (recursion <= 0) {
+		return [0, 0];
+	}
+		
+	var retv = undefined;
+	if (d >= curve.a && d < curve.a + 0.25) {
+		var d_ = (d - curve.a) * 4;
+		var adj = hilbert_1d_to_2d(d_, recursion - 1, curve.rotate_cc().mirror_x());
+		retv = [0 + adj[0]/2, 0 + adj[1]/2]; 
+	}
+	if (d >= curve.b && d < curve.b + 0.25) {
+		var d_ = (d - curve.b) * 4;
+		var adj = hilbert_1d_to_2d(d_, recursion - 1, curve);
+		retv = [0.5 + adj[0]/2, 0 + adj[1]/2]; 
+	}
+	if (d >= curve.c && d < curve.c + 0.25) {
+		var d_ = (d - curve.c) * 4;
+		var adj = hilbert_1d_to_2d(d_, recursion - 1, curve.rotate_c().mirror_x());
+		retv = [0 + adj[0]/2, 0.5 + adj[1]/2]; 
+	}
+	if (d >= curve.d && d < curve.d + 0.25) {
+		var d_ = (d - curve.d) * 4;
+		var adj = hilbert_1d_to_2d(d_, recursion - 1, curve);
+		retv = [0.5 + adj[0]/2, 0.5 + adj[1]/2]; 
+	}
+	
+	console.log(d, recursion, curve);	
+	console.log("retv: " + retv);
 	return retv;
 }
